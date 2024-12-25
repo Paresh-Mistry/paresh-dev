91575a2f0f06fc302f9ce3bc0905a5561319600c
Paresh-Mistry <paresh.mistry2004@gmail.com>
2024-12-25 15:09:58 +0530

    Updated site

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ['react-pdf'], // This explicitly tells Rollup to treat react-pdf as external
-    },
-  },
   plugins: [react()],
   base:"/paresh-dev"
 })
